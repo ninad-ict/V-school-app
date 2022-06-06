ba45ba990692bd5155a918c9749b00464927b429
Ninad <nskhanolkar@gmail.com>
2022-06-06 13:09:00 +0530

    zoom11

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const windmill = require('@windmill/react-ui/config')
 
 module.exports = windmill({
   // purge: ['src/**/*.js'],
+  mode: 'jit',
   purge:{
     content: ['src/**/*.js'],
     safelist:['mx-0','mx-1','mx-2','mx-3','mx-4','mx-5','mx-6','mx-8','mx-10','mx-12','mx-16','mx-20','mx-24','mx-32','mx-40','mx-48','mx-56','mx-64']
