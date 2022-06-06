cc6a672c115852c5c53d6c96830863c60bf10b80
Ninad <nskhanolkar@gmail.com>
2022-06-06 12:48:18 +0530

    zoom8

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,7 @@ module.exports = windmill({
   // purge: ['src/**/*.js'],
   purge:{
     content: ['src/**/*.js'],
-    safelist:[0,1,2,3,4,5,6,8,10,12,16,20,24,32,40,48,56,64].map(v=>{
-      return "mx-"+v;
-    })
+    safelist:['mx-0','mx-1','mx-2','mx-3','mx-4','mx-5','mx-6','mx-8','mx-10','mx-12','mx-16','mx-20','mx-24','mx-32','mx-40','mx-48','mx-56','mx-64']
   },
   theme: {
     extend: {
